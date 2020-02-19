fb1db5d5a88f2dd98eef855f0e087bfd1fdad326
Shrewd <54885862+ShrewdStyle@users.noreply.github.com>
2020-02-19 21:05:20 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+
+// --------- SMOOTH SCROLLING ----------- //
+
+// Home
+function smoothScrollHome(){
+    document.querySelector('#top-slides').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+// Services
+function smoothScrollServices(){
+    document.querySelector('.services').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+// About
+function smoothScrollAbout(){
+    document.querySelector('#about-main').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+// News
+function smoothScrollNews(){
+    document.querySelector('#news-main').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+// Skills
+function smoothScrollSkills(){
+    document.querySelector('#skills').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+// Skills
+function smoothScrollContact(){
+    document.querySelector('#contact').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+
+// ---------------- CURRENT NAV VIEW -----------------//
+
